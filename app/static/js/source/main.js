7f8e257ca08103a4873d3f8ba3c3be2a08d2a916
Randall Russell <XWINGFIGHTER@RandoCalrissian.local>
2014-03-20 13:51:25 -0500

    chat window

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -1,3 +1,5 @@
+/* global io:true */
+
 (function(){
 
   'use strict';
@@ -6,7 +8,33 @@
 
   function initialize(){
     $(document).foundation();
+    initializeSocketIO();
+    $('button').click(sendMessage);
+  }
+  var socket;
+
+  function initializeSocketIO(){
+    socket = io.connect('/app');
+    socket.on('online', function(data){console.log(data);});
+    socket.on('message', addMessage);
   }
 
-})();
+  function addMessage(data){
+    var $div = $('div');
+    $div = data.text;
+    var $br = $('<br>');
+    $('#messages').append($div);
+    $('#messages').append($br);
+    var textArea = $('textarea');
+    textArea.val('');
+  }
 
+  function sendMessage(){
+    var a = {};
+    a.text = $('textarea').val();
+    socket.emit('newMessage', a);
+  }
+
+
+
+})();
